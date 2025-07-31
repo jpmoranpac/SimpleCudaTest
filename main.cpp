ed235bcdcbb5e5853f8b1691a516fbbd414a257d
jpmoranpac <88604302+jpmoranpac@users.noreply.github.com>
2025-08-01 07:33:20 +1000

    convert to CUDA C++

--- main.cpp
+++ main.cpp
@@ -1,7 +1,8 @@
 #include <iostream>
 #include <math.h>
 
-// function to add the elements of two arrays
+// Kernel function to add the elements of two arrays
+__global__
 void add(int n, float *x, float *y)
 {
     for (int i = 0; i < n; i++) {
@@ -9,21 +10,25 @@ void add(int n, float *x, float *y)
     }
 }
 
-int main(void)
-{
+int main(void) {
     int N = 1<<20; // 1M elements
 
-    float *x = new float[N];
-    float *y = new float[N];
+    // Allocate Unified Memory -- accessible from CPU or GPU
+    float *x, *y, *sum;
+    cudaMallocManaged(&x, N*sizeof(float));
+    cudaMallocManaged(&y, N*sizeof(float));
 
-    // initialize x and y arrays on the host
+    // Initialize x and y arrays on the host
     for (int i = 0; i < N; i++) {
         x[i] = 1.0f;
         y[i] = 2.0f;
     }
 
     // Run kernel on 1M elements on the CPU
-    add(N, x, y);
+    add<<<1, 1>>>(N, sum, x, y);
+
+    // Wait for GPU to finish before accessing on host
+    cudaDeviceSynchronize();
 
     // Check for errors (all values should be 3.0f)
     float maxError = 0.0f;
@@ -32,8 +37,8 @@ int main(void)
     std::cout << "Max error: " << maxError << std::endl;
 
     // Free memory
-    delete [] x;
-    delete [] y;
+    cudaFree(x);
+    cudaFree(y);
 
     return 0;
 }
